c30414e41eb7b405379be3b46caf4ea7a8197b4b
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2023-02-08 13:24:45 +0100

    Add files via upload

---
+++ src/trivia.py
@@ -0,0 +1,6 @@
+def quiz():
+name = input('Whats your name? ')
+ans = 'yes'
+questions = ['How old is the earth?', 'Where was the first person that discover America?', 'How many sides does a heptagon have?']
+while ans == 'yes':
+    print(questions)
