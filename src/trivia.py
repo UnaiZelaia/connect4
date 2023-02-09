1fcf4c2e01fe582393867f0d9c22a84ec97621d3
hidalgosandra <114407178+hidalgosandra@users.noreply.github.com>
2023-02-09 14:20:32 +0100

    Delete trivia.py

--- src/trivia.py
+++
@@ -1,6 +0,0 @@
-def quiz():
-name = input('Whats your name? ')
-ans = 'yes'
-questions = ['How old is the earth?', 'Where was the first person that discover America?', 'How many sides does a heptagon have?']
-while ans == 'yes':
-    print(questions)
